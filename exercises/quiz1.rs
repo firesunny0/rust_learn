9a755d09ae8f1108769d4a606d6c58018a6471f8
firesunny0 <firesunny@hotmail.com>
2022-10-19 23:37:12 +0800

    point: string

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn ..... {
+fn calculate_apple_price(cnt: isize) -> isize {
+    match cnt {
+        0..=40 => cnt * 2,
+        _ => cnt,
+    }
+}
 
 // Don't modify this function!
 #[test]
