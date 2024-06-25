77e40dc45712724bbbf8729f278dc200ce80b5b1
firesunny0 <firesunny@hotmail.com>
2024-06-25 20:23:34 +0800

    :construction reboot again until enum

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,10 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = "You should be able to copy the contents of the array into a slice. 
+    You should be able to copy the contents of the array into a slice. ";
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity_bought : i32) -> i32 {
+    if quantity_bought > 40 {
+        quantity_bought 
+    } else {
+        quantity_bought * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
